258a1f12c65f2769da2461216c3ef9b8beeef849
Sairam Poosarla <Sairam.Poosarla@afpa.org.au>
2026-01-06 14:21:44 +1100

    select component added but has issues with displaying the dropdown

--- app/(logged-out)/sign-up/page.tsx
+++ app/(logged-out)/sign-up/page.tsx
@@ -6,13 +6,21 @@ import * as z from "zod";
 //import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Field, FieldGroup, FieldLabel, FieldDescription, FieldError } from "@/components/ui/field";
+import { Field, FieldGroup, FieldLabel, FieldDescription, FieldError, FieldContent } from "@/components/ui/field";
 import {Input} from "@/components/ui/input";
+
+import { Select, SelectContent, SelectGroup, SelectTrigger, SelectValue, SelectItem, SelectLabel } from "@radix-ui/react-select";
 
 //here we are setting up the form schema
 //where we are defining the type of data for each feild
 const formSchema = z.object({
     email: z.email(),
+    //declaring the enum
+    accountType: z.enum(["personal", "company"]),
+    companyName: z.string().optional(),
+    //coerce is for converting a string to number
+    //we need to do this as by default we get the data in string format
+    numberOfEmployees: z.coerce.number().optional()
 })
 
 export default function LoginPage() {
@@ -49,6 +57,7 @@ export default function LoginPage() {
                         name = "email"
                         control={form.control}
                         render = {({field, fieldState}) => (
+                            // The main Field component. We use data-invalid for styling errors.
                             <Field data-invalid={fieldState.invalid ? "" : undefined}>
                             <FieldLabel htmlFor={field.name}>Email</FieldLabel>
                             <Input
@@ -61,16 +70,48 @@ export default function LoginPage() {
                                 {/* Show error message if validation fails */}
 
                             {fieldState.error && (
-
                                 <FieldError>{fieldState.error.message}</FieldError>
-
                             )}
                             </Field>
                         )}
                         />
 
+                        <Controller 
+                        name="accountType"
+                        control={form.control}
+                        render={({field, fieldState}) => (
+                            <Field
+                                orientation="responsive"
+                                data-invalid = {fieldState.invalid ? "this is error for select testing......" : undefined}
+                                >
+                                    <FieldContent>
+                                        <FieldLabel htmlFor={field.name}>
+                                            Account Type
+                                        </FieldLabel>
+                                        <FieldDescription>
+                                            this is the description for select field
+                                        </FieldDescription>
+                                        {fieldState.error && (<FieldError>{fieldState.error.message}</FieldError>)}
+                                        
+                                        <Select onValueChange={field.onChange}>
+                                            <SelectTrigger className="z-20">
+                                                <SelectValue placeholder="Select Account Type"/>
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                    <SelectGroup>
+                                                        <SelectLabel>Account Type</SelectLabel>
+                                                        <SelectItem value="personal"> Personal</SelectItem>
+                                                        <SelectItem value="company"> Company</SelectItem>
+                                                    </SelectGroup>
+                                            </SelectContent>
+                                        </Select>
+                                    </FieldContent>
+                                </Field>
+                        )}/>
+
+
                     </FieldGroup>
-                    <Button type="submit" className="w-full">
+                    <Button type="submit" className="w-full mt-6">
                         Sign Up
                     </Button>
                 </form>
