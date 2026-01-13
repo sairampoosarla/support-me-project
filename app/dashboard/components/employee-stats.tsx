0315d9e0e3f1caa93fef0e198e73a1d0ebfb7790
Sairam Poosarla <Sairam.Poosarla@afpa.org.au>
2026-01-13 18:42:24 +1100

    added some stats on total employees

--- app/dashboard/components/employee-stats.tsx
+++ app/dashboard/components/employee-stats.tsx
@@ -1,11 +1,37 @@
-import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { UserIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Employeestats() {
     return (
         <div className="grid lg:grid-cols-3 gap-4">
-            <Card>Card 1</Card>
-            <Card>Card 2</Card>
-            <Card>Card 3</Card>
+            <Card>
+                <CardHeader>
+                <CardTitle className="text-base">Total Employees</CardTitle>
+                </CardHeader>
+                <CardContent className="flex justify-between items-center gap-4">
+                    <div className="flex gap-2">
+                    <UserIcon />
+                    <p className="text-5xl font-bold">10</p>
+                    </div>
+                    <div>
+                        <Button size="sm" asChild>
+                            <Link href="/dashboard/employees">View all</Link>
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader>
+                <CardTitle className="text-base">Total Employees</CardTitle>
+                </CardHeader>
+            </Card>
+            <Card className="border-pink-500">
+                <CardHeader>
+                <CardTitle className="text-base">Total Employees</CardTitle>
+                </CardHeader>
+            </Card>
         </div>
     );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import Employeestats from "./components/employee-stats";
 export default function DashboardPage() {
     return (
         //here we are creating tabs for Employees and Teams
-        <Tabs>
+        <Tabs defaultValue="employees">
             {/* Here we are defining the tab list with two tabs: Employees and Teams */}
             {/* Each tabTrigger needs to have a value prop which should match the corresponding TabsContent value so respective information is displayed */}
             <TabsList className="mb-4">
