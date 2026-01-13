93a3d912f166c3a6539626a4af3e809a5e177b86
Sairam Poosarla <Sairam.Poosarla@afpa.org.au>
2026-01-13 11:52:48 +1100

    added new folder and files for each menu item

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,20 +1,35 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import MenuItem from "./menu-item";
 import MenueTitle from "./menu-title";
+import Link from "next/link";
+import { LayoutDashboard } from "lucide-react";
 
 export default function MainMenu() {
     return (
-        <div className="bg-muted overflow-auto p-4">
+        //flex and flex-col are added to make the menu items stack vertically
+        <div className="bg-muted overflow-auto p-4 flex flex-col">
             {/* Menu Title Component is added here */}
             <div className="border-b pb-4 border-b-zinc-300">
                 <MenueTitle />
             </div>
             {/* Each Menu Items are added here */}
-            <div className="py-4">
+            <div className="grow py-4">
+                <div>
+                    <LayoutDashboard/>
                 <MenuItem href="/dashboard">Dashboard</MenuItem>
+                </div>
                 <MenuItem href="/dashboard/teams">Teams</MenuItem>
                 <MenuItem href="/dashboard/employees">Employees</MenuItem>
                 <MenuItem href="/dashboard/account">Account</MenuItem>
                 <MenuItem href="/dashboard/settings">Settings</MenuItem>
+            </div>
+            {/* Menu Footer Component is added here */}
+            {/*flex is added so that avatar and text are in same line*/}
+            <div className="flex items-center gap-2 justify-between ">
+                <Avatar>
+                    <AvatarFallback className="bg-pink-300">TP</AvatarFallback>
+                </Avatar>
+                <Link href="/" className="hover:underline">Logout</Link>
             </div>
         </div>
     )
--- app/dashboard/components/menu-item.tsx
+++ app/dashboard/components/menu-item.tsx
@@ -15,6 +15,8 @@ type Props = {
 export default function MenuItem({children, href}:Props) {
 
     const pathName = usePathname();
+    //here we are checking if the current path is same as href passed to the component i.e., if the menu item is loaded on the page or not
     const isActive = pathName === href;
+    //based on the active status we are applying different styles to the menu item so that the active menu item is highlighted
     return <Link className={isActive ? cn("block p-2 bg-primary rounded-md text-white hover:text-white hover:bg-primary") :  cn("p-2 block hover:bg-white rounded-md text-muted-foreground hover:text-foreground")}href={href}>{children}</Link>;
 }
